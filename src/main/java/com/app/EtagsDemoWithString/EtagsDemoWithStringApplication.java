8a9def112cc6124ef9762263b017b2a3945a1240
Iftekharuddin Syed <syedy636@gmail.com>
2024-08-27 19:54:55 +0200

    init commit

---
+++ src/main/java/com/app/EtagsDemoWithString/EtagsDemoWithStringApplication.java
@@ -0,0 +1,13 @@
+package com.app.EtagsDemoWithString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtagsDemoWithStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtagsDemoWithStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/EtagsDemoWithString/EtagsDemoWithStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.EtagsDemoWithString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtagsDemoWithStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
